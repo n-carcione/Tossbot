6daa0b1066c6a1422e1aaaf465bf76f3088f6b86
n-carcione <97858680+n-carcione@users.noreply.github.com>
2022-05-03 19:47:05 -0400

    Add ball detection, add joint velocity limits, refactor

--- run_dynamic_joints_tossbot.py
+++ run_dynamic_joints_tossbot.py
@@ -6,6 +6,7 @@ from frankapy import FrankaConstants as FC
 from frankapy.proto_utils import sensor_proto2ros_msg, make_sensor_group_msg
 from frankapy.proto import JointPositionSensorMessage, ShouldTerminateSensorMessage
 from franka_interface_msgs.msg import SensorDataGroup
+from frankapy.utils import convert_array_to_rigid_transform
 
 from frankapy.utils import min_jerk
 
@@ -20,159 +21,143 @@ import tossbot_utils as tu
 
 if __name__ == "__main__":
     fa = FrankaArm()
+    thrown = True
+    AZURE_KINECT_INTRINSICS = 'calib_tossbot/azure_kinect.intr'
+    AZURE_KINECT_EXTRINSICS = 'calib_tossbot/azure_kinect_overhead/azure_kinect_overhead_to_world.tf'
+
+    cv_bridge = CvBridge()
+    azure_kinect_intrinsics = CameraIntrinsics.load(AZURE_KINECT_INTRINSICS)
+    azure_kinect_to_world_transform = RigidTransform.load(AZURE_KINECT_EXTRINSICS)
     while True:
         fa.stop_skill()
         fa.reset_joints()
-        fa.close_gripper()
+        fa.open_gripper()
 
         #GET STUFF FROM PERCEPTION
         '''============================================================================'''
-        print('Opening the eyes!!')
-
         rospy.sleep(1)
-        AZURE_KINECT_INTRINSICS = 'calib_tossbot/azure_kinect.intr'
-        AZURE_KINECT_EXTRINSICS = 'calib_tossbot/azure_kinect_overhead/azure_kinect_overhead_to_world.tf'
 
-        cv_bridge = CvBridge()
-        azure_kinect_intrinsics = CameraIntrinsics.load(AZURE_KINECT_INTRINSICS)
-        azure_kinect_to_world_transform = RigidTransform.load(AZURE_KINECT_EXTRINSICS)
+        (ball_centers, ball_world_frame, azure_kinect_rgb_image,ball_contours) = locate_balls(cv_bridge, azure_kinect_intrinsics, azure_kinect_to_world_transform)
 
-        (center,target_world_frame,azure_kinect_rgb_image,contour) = locate_container(cv_bridge,
-                                                                        azure_kinect_intrinsics,
-                                                                        azure_kinect_to_world_transform)
+        #Only execute ball pickup and throw if there is a ball detected
+        while ball_centers:
+            fa.stop_skill()
+            fa.reset_joints()
+            if thrown:
+                (ball_centers, ball_world_frame, azure_kinect_rgb_image, ball_contours) = locate_balls(cv_bridge, azure_kinect_intrinsics, azure_kinect_to_world_transform)
+                #Show all the ball detections
+                for bc in ball_centers:
+                    all_balls_rgb_image = cv2.circle(azure_kinect_rgb_image, (int(bc[0]),int(bc[1])), radius=10, color=(255, 255, 255), thickness=-1)
+                for cont in ball_contours:
+                    cv2.drawContours(all_balls_rgb_image, cont, -1, (0,0,0), 20)
+                all_balls_rgb_image = cv2.resize(all_balls_rgb_image, (600, 400))  
+                cv2.imshow("image", all_balls_rgb_image)
+                cv2.waitKey(1500)
+            #Only consider something to be a ball if it is 0.1m - 0.8m in front of the robot
+            #This helps avoid false detections from the slightly green 8020 used to 
+            #construct the table for the arm
+            if ball_world_frame[0][0] > 0.1 and ball_world_frame[0][0] < 0.5 and ball_world_frame[0][1] > -0.4 and ball_world_frame[0][1] < 0.4:                
+                #draw targetted ball on image
+                target_ball_rgb_image = cv2.circle(azure_kinect_rgb_image, (int(ball_centers[0][0]),int(ball_centers[0][1])), radius=10, color=(255, 255, 255), thickness=-1)
+                cv2.drawContours(target_ball_rgb_image, ball_contours[0], -1, (0,255,255), 20)
+                cv2.namedWindow("image")
+                target_ball_rgb_image = cv2.resize(target_ball_rgb_image, (600, 400))  
+                cv2.imshow("image", target_ball_rgb_image)
+                cv2.waitKey(1500)
                 
-        print(f"Transformed\nx: {target_world_frame[0]}, y: {target_world_frame[1]}")
+                (center,target_world_frame,azure_kinect_rgb_image,contour) = locate_container(cv_bridge,
+                                                                                azure_kinect_intrinsics,
+                                                                                azure_kinect_to_world_transform)
 
-        # draw center on image
-        azure_kinect_rgb_image = cv2.circle(azure_kinect_rgb_image, (int(center[0]),int(center[1])), radius=10, color=(255, 255, 255), thickness=-1)
+                # draw detected bin on image
+                azure_kinect_rgb_image = cv2.circle(azure_kinect_rgb_image, (int(center[0]),int(center[1])), radius=10, color=(255, 255, 255), thickness=-1)
+                cv2.drawContours(azure_kinect_rgb_image, contour, -1, (0,0,0), 20)
+                cv2.namedWindow("image")
+                azure_kinect_rgb_image = cv2.resize(azure_kinect_rgb_image, (600, 400))  
+                cv2.imshow("image", azure_kinect_rgb_image)
+                cv2.waitKey(1500)
+                '''========================================================================'''
 
-        # draw the biggest contour (c) in green
-        #cv2.rectangle(red_mask,(x,y),(x+w,y+h),(0,255,0),2)
-        cv2.drawContours(azure_kinect_rgb_image, contour, -1, (0,0,0), 20)
+                target_location = target_world_frame
+                theta_1 = tu.get_joint_1(target_location)
+
+                #Joint angles 1, 3, 5, and 7 are the same as the reset angles
+                #Only change joint angles 2, 4, 6
+                stationary_joints = [1,3,5,7]
+                moving_joints = [2,4,6]
+                #Starting and release joint values for an underhand throw
+                joints_init = [theta_1, -0.4431077, 0.0, -2.86279262, 0.0, 1.80853718, 7.85244335e-01]
+                joints_rel = [theta_1, 0.11036068, 0.0, -2.07947407, 0.0, 2.65808482, 7.85244335e-01]
+                #Compute Jacobian at release
+                rj = fa.get_jacobian(joints_rel)
+
+                #Compute forward kinematics at release position to find (x,y) position
+                #of end effector.  Use this to calculate distance ball needs to be thrown
+                transforms = fa.get_links_transforms(joints_rel, use_rigid_transforms=False)
+                ee_pos = transforms[-1, :3, 3]
+                dist = math.sqrt( (target_location[0]-ee_pos[0])**2 + (target_location[1]-ee_pos[1])**2 )
+                print('Distance to throw: {}m'.format(dist))
+                
+                vel_rel = tu.get_release_velocity(dist, theta_1, ee_pos[2])
+                release_joint_vel = tu.get_release_joint_velocity(vel_rel, rj, stationary_joints, moving_joints)
+                
+                # Check to make sure the target is within the throwing range (joint velocities don't
+                # violate defined constraints)
+                if not tu.check_joint_vels(release_joint_vel):
+                    print("Target out of range")
+                    thrown = False
+                    fa.reset_joints()
+                    break
+
+                #Grab the first ball detected
+                ball_pose = tu.get_ball_grab_pose(ball_world_frame[0])
+                fa.goto_pose(convert_array_to_rigid_transform(ball_pose),ignore_virtual_walls=True)
+                fa.close_gripper()
+
+                tr = 1.0
+                (joints_traj, c, T) = tu.gen_polynomials(joints_init, joints_rel, release_joint_vel, tr)
+                
+                while tu.exists_vel_violation(c, tr):
+                    tr = tr + 0.1
+                    (joints_traj, c, T) = tu.gen_polynomials(joints_init, joints_rel, release_joint_vel, tr)
 
 
-        cv2.namedWindow("image")
-        azure_kinect_rgb_image = cv2.resize(azure_kinect_rgb_image, (1160, 740))  
-        cv2.imshow("image", azure_kinect_rgb_image)
-        #cv2.setMouseCallback("image", onMouse, object_image_position)
-        cv2.waitKey()
+                thrown = True
+                ball_centers.remove(ball_centers[0])
+                ball_world_frame.remove(ball_world_frame[0])
+                ball_contours.remove(ball_contours[0])
 
-        '''========================================================================'''
-        target_location = target_world_frame
-        theta_1 = tu.get_joint_1(target_location)
+                dt = 0.01
+                ts = np.arange(0, T, dt)
 
-        #Joint angles 1, 3, 5, and 7 are the same as the reset angles
-        #Only change joint angles 2, 4, 6
-        stationary_joints = [1,3,5,7]
-        moving_joints = [2,4,6]
-        #Underhand
-        joints_init = [theta_1, -0.4431077, 0.0, -2.86279262, 0.0, 1.80853718, 7.85244335e-01]
-        joints_rel = [theta_1, 0.11036068, 0.0, -2.07947407, 0.0, 2.65808482, 7.85244335e-01]
-        transforms = fa.get_links_transforms(joints_rel, use_rigid_transforms=False)
-        ee_pos = transforms[-1, :3, 3]
-        rj = fa.get_jacobian(joints_rel)
+                fa.goto_joints(joints_traj[0])
+                # rospy.loginfo('Initializing Sensor Publisher')
+                pub = rospy.Publisher(FC.DEFAULT_SENSOR_PUBLISHER_TOPIC, SensorDataGroup, queue_size=1000)
+                rate = rospy.Rate(1 / dt)
 
-        dist = math.sqrt( (target_location[0]-ee_pos[0])**2 + (target_location[1]-ee_pos[1])**2 )
+                # rospy.loginfo('Publishing joints trajectory...')
+                # To ensure skill doesn't end before completing trajectory, make the buffer time much longer than needed
+                fa.goto_joints(joints_traj[0], duration=T, dynamic=True, buffer_time=10)
+                init_time = rospy.Time.now().to_time()
+                for i in range(1, len(ts)-1):
+                    traj_gen_proto_msg = JointPositionSensorMessage(
+                        id=i, timestamp=rospy.Time.now().to_time() - init_time, 
+                        joints=joints_traj[i]
+                    )
+                    ros_msg = make_sensor_group_msg(
+                        trajectory_generator_sensor_msg=sensor_proto2ros_msg(
+                            traj_gen_proto_msg, SensorDataMessageType.JOINT_POSITION)
+                    )
 
-        vel_rel = tu.get_release_velocity(dist, theta_1, ee_pos[2])
-        print(vel_rel)
-        release_joint_vel = tu.get_release_joint_velocity(vel_rel, rj, stationary_joints, moving_joints)
-        print(release_joint_vel)
-        tr = 1.0
-        tf = 2.0
-        dt = 0.01
-
-        q20 = joints_init[1]
-        q2r = joints_rel[1]
-        q2f = 0.36
-        q2r_dot = release_joint_vel[1]
-
-        q40 = joints_init[3]
-        q4r = joints_rel[3]
-        q4f = -1.6
-        q4r_dot = release_joint_vel[3]
-
-        q60 = joints_init[5]
-        q6r = joints_rel[5]
-        q6f = 3.1
-        q6r_dot = release_joint_vel[5]
-
-        Ar = [[0, 0, 0, 1], [0, 0, 1, 0], [tr**3, tr**2, tr, 1], [3*tr**2, 2*tr, 1, 0]]
-        Af = [[tr**3, tr**2, tr, 1], [3*tr**2, 2*tr, 1, 0], [tf**3, tf**2, tf, 1], [3*tf**2, 2*tf, 1, 0]]
-
-        b2r = [q20, 0, q2r, q2r_dot]
-        b2f = [q2r, q2r_dot, q2f, 0]
-        b4r = [q40, 0, q4r, q4r_dot]
-        b4f = [q4r, q4r_dot, q4f, 0]
-        b6r = [q60, 0, q6r, q6r_dot]
-        b6f = [q6r, q6r_dot, q6f, 0]
-
-        c2r = np.linalg.inv(Ar) @ b2r
-        c2f = np.linalg.inv(Af) @ b2f
-        c4r = np.linalg.inv(Ar) @ b4r
-        c4f = np.linalg.inv(Af) @ b4f
-        c6r = np.linalg.inv(Ar) @ b6r
-        c6f = np.linalg.inv(Af) @ b6f
-
-        joints_traj = []
-
-        q1 = joints_init[0]
-        q3 = joints_init[2]
-        q5 = joints_init[4]
-        q7 = joints_init[6]
-
-        for t in np.arange(0,tf,dt):
-            if (t <= tr):
-                q2= c2r[0]*t**3 + c2r[1]*t**2 + c2r[2]*t + c2r[3] 
-                q4 = c4r[0]*t**3 + c4r[1]*t**2 + c4r[2]*t + c4r[3]
-                q6 = c6r[0]*t**3 + c6r[1]*t**2 + c6r[2]*t + c6r[3]
-                joints_traj.append([q1, q2, q3, q4, q5, q6, q7])
+                    #Release point is actually at i==100 but there is a slight delay
+                    #Reasonable performance found when i==85
+                    if i == 85:
+                        fa.goto_gripper(0.08, speed=1.2 , block=False)
+                    
+                    pub.publish(ros_msg)
+                    rate.sleep()
             else:
-                q2 = c2f[0]*t**3 + c2f[1]*t**2 + c2f[2]*t + c2f[3]
-                q4 = c4f[0]*t**3 + c4f[1]*t**2 + c4f[2]*t + c4f[3]
-                q6 = c6f[0]*t**3 + c6f[1]*t**2 + c6f[2]*t + c6f[3]
-                joints_traj.append([q1, q2, q3, q4, q5, q6, q7])
-        
-        T = 2
-        dt = 0.01
-        ts = np.arange(0, T, dt)
-
-        fa.goto_joints(joints_traj[0])
-        rospy.loginfo('Initializing Sensor Publisher')
-        pub = rospy.Publisher(FC.DEFAULT_SENSOR_PUBLISHER_TOPIC, SensorDataGroup, queue_size=1000)
-        rate = rospy.Rate(1 / dt)
-
-        rospy.loginfo('Publishing joints trajectory...')
-        # To ensure skill doesn't end before completing trajectory, make the buffer time much longer than needed
-        fa.goto_joints(joints_traj[0], duration=T, dynamic=True, buffer_time=10)
-        init_time = rospy.Time.now().to_time()
-        for i in range(1, len(ts)-1):
-            traj_gen_proto_msg = JointPositionSensorMessage(
-                id=i, timestamp=rospy.Time.now().to_time() - init_time, 
-                joints=joints_traj[i]
-            )
-            ros_msg = make_sensor_group_msg(
-                trajectory_generator_sensor_msg=sensor_proto2ros_msg(
-                    traj_gen_proto_msg, SensorDataMessageType.JOINT_POSITION)
-            )
-
-            if i == 100:
-                fa.goto_gripper(0.08, speed=1.2 , block=False)
-            
-            rospy.loginfo('Publishing: ID {}'.format(traj_gen_proto_msg.id))
-            pub.publish(ros_msg)
-            rate.sleep()
-
-        # Stop the skill
-        # Alternatively can call fa.stop_skill()
-        term_proto_msg = ShouldTerminateSensorMessage(timestamp=rospy.Time.now().to_time() - init_time, should_terminate=True)
-        ros_msg = make_sensor_group_msg(
-            termination_handler_sensor_msg=sensor_proto2ros_msg(
-                term_proto_msg, SensorDataMessageType.SHOULD_TERMINATE)
-            )
-
-        pub.publish(ros_msg)
-
-        rospy.loginfo('Done')
-
-    
+                thrown = False
+                ball_centers.remove(ball_centers[0])
+                ball_world_frame.remove(ball_world_frame[0])
+                ball_contours.remove(ball_contours[0])
--- tossbot_utils.py
+++ tossbot_utils.py
@@ -4,7 +4,10 @@ import numpy as np
 def get_joint_1(target_location):
     # Use the x- and y-values of the target in the robot-base frame to calculate
     # what the value for the first joint should be
-    return math.atan2(target_location[1],target_location[0])
+    #With how the camera is set up, the center is off on the y
+    #(The camera sees the right side of the bin and factors this into the 
+    #estimation).  Shift the center a little left to account for this
+    return math.atan2(target_location[1]-0.02,target_location[0])
 
 def get_release_velocity(d, theta_1, h0):
     # Calculate the necessary release velocity so that the ball will
@@ -35,3 +38,128 @@ def get_release_joint_velocity(vel_rel, rj, stationary_joints, moving_joints):
 
 
     return release_joint_vel
+
+def get_ball_grab_pose(ball_loc):
+    reset_rot_mat = [[1, 0, 0], [0, -1, 0], [0, 0, -1]]
+    ball_pose = np.zeros([4,4])
+    ball_pose[:3,:3] = reset_rot_mat
+    #x-position should have a 0.01 offset as determined by testing
+    ball_pose[0, 3] = ball_loc[0] + 0.01
+    #y-position needs a 0.02 offset as determined by testing
+    ball_pose[1, 3] = ball_loc[1] + 0.02
+    #Ball will always be at this height as defined by set up
+    ball_pose[2, 3] = 0.0275
+    ball_pose[3,3] = 1
+
+    return ball_pose.flatten()
+
+def check_joint_vels(vels):
+    vel_constraints = [2.1750, 2.1750, 2.1750, 2.1750, 2.6100, 2.6100, 2.6100]
+    for i in range(len(vels)):
+        if vels[i] > vel_constraints[i]:
+            print('Velocity constraint at joint {}'.format(i+1))
+            print('Joint {} requires velocity of {}'.format(i+1, vels[i]))
+            return False
+    return True
+
+def get_endpoints(joints_init, joints_rel, release_joint_vel):
+    q20 = joints_init[1]
+    q2r = joints_rel[1]
+    q2f = 0.36
+    q2r_dot = release_joint_vel[1]
+
+    q40 = joints_init[3]
+    q4r = joints_rel[3]
+    q4f = -1.6
+    q4r_dot = release_joint_vel[3]
+
+    q60 = joints_init[5]
+    q6r = joints_rel[5]
+    q6f = 3.1
+    q6r_dot = release_joint_vel[5]
+
+    endpoints = [q20, q2r, q2f, q2r_dot, q40, q4r, q4f, q4r_dot, q60, q6r, q6f, q6r_dot]
+
+    return endpoints
+
+def get_A(tr, tf):
+    Ar = [[0, 0, 0, 1], [0, 0, 1, 0], [tr**3, tr**2, tr, 1], [3*tr**2, 2*tr, 1, 0]]
+    Af = [[tr**3, tr**2, tr, 1], [3*tr**2, 2*tr, 1, 0], [tf**3, tf**2, tf, 1], [3*tf**2, 2*tf, 1, 0]]
+
+    A = [Ar, Af]
+    return A
+
+def get_b(ep):
+    b2r = [ep[0], 0, ep[1], ep[3]]
+    b2f = [ep[1], ep[3], ep[2], 0]
+    b4r = [ep[4], 0, ep[5], ep[7]]
+    b4f = [ep[5], ep[7], ep[6], 0]
+    b6r = [ep[8], 0, ep[9], ep[11]]
+    b6f = [ep[9], ep[11], ep[10], 0]
+
+    b = [b2r, b2f, b4r, b4f, b6r, b6f]
+
+    return b
+
+def calc_c(A, b):
+    c2r = np.linalg.inv(A[0]) @ b[0]
+    c2f = np.linalg.inv(A[1]) @ b[1]
+    c4r = np.linalg.inv(A[0]) @ b[2]
+    c4f = np.linalg.inv(A[1]) @ b[3]
+    c6r = np.linalg.inv(A[0]) @ b[4]
+    c6f = np.linalg.inv(A[1]) @ b[5]
+
+    c = [c2r, c2f, c4r, c4f, c6r, c6f]
+
+    return c
+
+def gen_polynomials(joints_init, joints_rel, release_joint_vel, tr):
+    tf = tr + 1.0
+    dt = 0.01
+
+    endpoints = get_endpoints(joints_init, joints_rel, release_joint_vel)
+
+    A = get_A(tr, tf)
+    b = get_b(endpoints)
+    c = calc_c(A,b)
+
+    joints_traj = []
+
+    q1 = joints_init[0]
+    q3 = joints_init[2]
+    q5 = joints_init[4]
+    q7 = joints_init[6]
+
+    for t in np.arange(0,tf,dt):
+        if (t <= tr):
+            q2 = c[0][0]*t**3 + c[0][1]*t**2 + c[0][2]*t + c[0][3] 
+            q4 = c[2][0]*t**3 + c[2][1]*t**2 + c[2][2]*t + c[2][3]
+            q6 = c[4][0]*t**3 + c[4][1]*t**2 + c[4][2]*t + c[4][3]
+            joints_traj.append([q1, q2, q3, q4, q5, q6, q7])
+        else:
+            q2 = c[1][0]*t**3 + c[1][1]*t**2 + c[1][2]*t + c[1][3]
+            q4 = c[3][0]*t**3 + c[3][1]*t**2 + c[3][2]*t + c[3][3]
+            q6 = c[5][0]*t**3 + c[5][1]*t**2 + c[5][2]*t + c[5][3]
+            joints_traj.append([q1, q2, q3, q4, q5, q6, q7])
+
+    return (joints_traj, c, tf)
+
+def exists_vel_violation(c, tr):
+    tf = tr + 1.0
+    dt = 0.01
+
+    for t in np.arange(0,tf,dt):
+        if (t <= tr):
+            q2dot = 3*c[0][0]*t**2 + 2*c[0][1]*t + c[0][2]
+            q4dot = 3*c[2][0]*t**2 + 2*c[2][1]*t + c[2][2]
+            q6dot = 3*c[4][0]*t**2 + 2*c[4][1]*t + c[4][2]
+            if q2dot > 2.175 or q4dot > 2.175 or q6dot > 2.61:
+                return True
+        else:
+            q2dot = 3*c[1][0]*t**2 + 2*c[1][1]*t + c[1][2]
+            q4dot = 3*c[3][0]*t**2 + 2*c[3][1]*t + c[3][2]
+            q6dot = 3*c[5][0]*t**2 + 2*c[5][1]*t + c[5][2]
+            if q2dot > 2.175 or q4dot > 2.175 or q6dot > 2.61:
+                return True
+
+    return False
